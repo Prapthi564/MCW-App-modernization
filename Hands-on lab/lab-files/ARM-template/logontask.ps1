f173f55463df9739503046c326e0454c6c557bc3
shashankms9 <104433795+shashankms9@users.noreply.github.com>
2024-11-13 11:19:18 +0530

    Create logontask.ps1

---
+++ Hands-on lab/lab-files/ARM-template/logontask.ps1
@@ -0,0 +1,26 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\logontask.txt -Append
+
+# Define the download URL and the local file path
+$downloadUrl = "https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.45.8999.1.msi"
+$installerPath = "$env:TEMP\IntegrationRuntime.msi"
+ 
+# Download the MSI file
+Write-Output "Downloading the MSI package..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
+ 
+# Check if the file was downloaded successfully
+if (Test-Path $installerPath) {
+    Write-Output "Download completed. Installing the package..."
+ 
+    # Install the MSI package silently
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$installerPath`" /quiet /norestart" -Wait
+ 
+    Write-Output "Installation completed."
+ 
+    # Clean up the installer file if desired
+    Remove-Item -Path $installerPath -Force
+} else {
+    Write-Output "Failed to download the MSI package."
+}
+
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
